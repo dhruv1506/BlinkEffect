66d7155db8199537dfe7748881abfa334a8599bf
dhruv <dhruv.nkncp@nic.in>
2021-11-10 14:29:45 +0530

    Commit by Dhruv 11-10-2021 14:29

---
+++ BlinkLibrary/src/main/java/in/nic/blinklibrary/BlinkEffect.kt
@@ -0,0 +1,26 @@
+package  `in`.nic.blinklibrary
+
+import android.animation.Animator
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.view.View
+import android.view.animation.Animation
+
+object BlinkEffect {
+
+    fun blin(view: View)
+    {
+            val animator: ObjectAnimator=ObjectAnimator.ofInt(
+
+                view ,"backgroundColor",Color.YELLOW,
+                Color.RED,Color.GREEN
+            )
+            animator.duration=500
+            animator.setEvaluator(ArgbEvaluator())
+            animator.repeatCount=Animation.REVERSE
+            animator.repeatCount=Animation.INFINITE
+            animator.start()
+
+    }
+}
